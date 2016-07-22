c1e0e3e0804ce8c7bdde454702994aa73b38a898
sanoyo <sa178kla@docomo.ne.jp>
2016-07-22 15:44:17 +0000

    first commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160714143323) do
+ActiveRecord::Schema.define(version: 20160721033114) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "name"
     t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "age"
   end
 
 end
